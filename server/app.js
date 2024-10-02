08308ddfd5658e53a9f12660353068b4bcc9f402
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 12:15:41 -0300

    spotify

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import conectDb from './db.js';
+ import conectDb from './db.js';
 import cors from 'cors'
+import artista from './models/Artista.js';
 
 const app = express();
-app.use(cors());
-const conexao = await conectDb()
+app.use(cors(), express.json());
+ const conexao = await conectDb()
 
-conexao.on('error', (erro) => {
-    console.log('Erro ao conectar', erro)
-});
+ conexao.on('error', (erro) => {
+     console.log('Erro ao conectar', erro)
+ });
 
-conexao.once('open', () => {
-    console.log('Conectando no MongoDB')
+ conexao.once('open', () => {
+     console.log('Conectando no MongoDB')
+ })
+
+app.get("/artistas", async (req, res) => {
+    const listaArtistas = await artista.find({});
+    res.status(200).json(listaArtistas)
 })
 
 app.listen(3000, () => {
