da148985a6a113719c2591fb7991bb7ea948ff01
RaulLaguna2310 <raullaguna2310@gmail.com>
2024-11-01 23:17:02 -0300

    finalizado2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from "react"
 import CardSideBar from "./components/CardSideBar"
 import Container from "./components/Container"
 import Header from "./components/Header"
-import Main from "./components/Main"
 import SideBar from "./components/SideBar"
-import { useEffect } from "react"
-import artista from "../server/models/Artista"
 import { Outlet } from "react-router-dom"
 
 function App() {
 
-    
-
   return (
-
     <>
-      <Header/>
-     <Container>
+      <Header />
+      <Container>
         <SideBar>
-          <CardSideBar/>
+          <CardSideBar />
         </SideBar>
-          <Outlet/>
+        <Outlet />
       </Container>
-        
-      
     </>
   )
 }
