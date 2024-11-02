089dfc76e3ceb1254fb1093802b7eeafb0986f80
RaulLaguna2310 <raullaguna2310@gmail.com>
2024-11-01 23:30:46 -0300

    finalizado4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Container from "./components/Container"
 import Header from "./components/Header"
 import SideBar from "./components/SideBar"
 import { Outlet } from "react-router-dom"
+import { useEffect } from "react";
+
 
 function App() {
 
